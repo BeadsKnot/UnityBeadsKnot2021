f054e958fcb67edf7d0d291f33c6e5c669550ee8
Kazushi Ahara <ahara@meiji.ac.jp>
2021-01-11 17:48:50 +0900

    マウス入力部分

---
+++ UnityBeadsKnot2021/Assets/User.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public Vector3 mouse;
+    public Vector3 previousPosition;
+
+    public List<Vector3> trace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Screen_size = (Screen.width, Screen.height);
+        trace = new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            trace.Clear();
+            previousPosition = mouse;
+        }
+        else if (Input.GetMouseButton(0))
+		{
+			if ((previousPosition - mouse).magnitude >= 10)
+			{
+                trace.Add(mouse);
+                previousPosition = mouse;
+            }
+		}
+        else if (Input.GetMouseButtonUp(0))
+		{
+            AddEdge(trace);
+		}
+    }
+
+    void AddEdge(List<Vector3> t)
+	{
+
+	}
+}
