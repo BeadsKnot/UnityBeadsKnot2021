38ed66a231756000daf6f2bb58a044c213963d82
Kazushi Ahara <ahara@meiji.ac.jp>
2021-01-12 18:10:13 +0900

    Beadを作った

---
+++ UnityBeadsKnot2021/Assets/Bead.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bead : MonoBehaviour
+{
+    GameObject Nnorth, Nsouth, Uwest, Ueast;
+    GameObject NnorthNode, NsouthNode, UwestNode, UeastNode;
+    bool isNode, isCrossing, isEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isNode = false;
+        isCrossing = false;
+        isEnd = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CopyBead(Bead b)
+	{
+        Nnorth = b.Nnorth;
+        Nsouth = b.Nsouth;
+        Uwest = b.Uwest;
+        Ueast = b.Ueast;
+        NnorthNode = b.NnorthNode;
+        NsouthNode = b.NsouthNode;
+        UwestNode = b.UwestNode;
+        UeastNode = b.UeastNode;
+    }
+}
--- UnityBeadsKnot2021/Assets/ShortLine.cs
+++ UnityBeadsKnot2021/Assets/ShortLine.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ShortLine : MonoBehaviour
 {
+    public Bead bd0, bd1;
     public Vector3 vec0, vec1;
     // Start is called before the first frame update
     void Start()
@@ -15,13 +16,13 @@ public class ShortLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        vec0 = bd0.transform.position;
+        vec1 = bd1.transform.position;
         Vector3 vecM = (vec0 + vec1) * 0.5f;
         vecM.z = -0.1f;
         transform.position = vecM;
-        float len = (vec1 - vec0).magnitude;
-        transform.localScale = new Vector3(len, 0.02f, 1f);
-        float th = Mathf.Rad2Deg * Mathf.Atan2(vec1.y - vec0.y, vec1.x - vec0.x);
-        Quaternion rotation = Quaternion.Euler(0f, 0f, th);
-        transform.rotation = rotation;
+        transform.localScale = new Vector3((vec1 - vec0).magnitude, 0.02f, 1f);
+        transform.rotation = Quaternion.Euler(0f, 0f, 
+            Mathf.Rad2Deg * Mathf.Atan2(vec1.y - vec0.y, vec1.x - vec0.x));
     }
 }
